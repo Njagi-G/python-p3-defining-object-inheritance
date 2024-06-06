2526fe951d23c3d3f5dd13504066eebd708a7bf5
Njagi Githaiga <njagigithaiga@gmail.com>
2024-06-06 19:07:11 +0300

    Complete lab by setting up inheritance.

--- lib/car.py
+++ lib/car.py
@@ -2,3 +2,13 @@ from vehicle import Vehicle
 
 class Car(Vehicle):
     pass
+
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+
+sedan = Car(10, 25)
+print(sedan.wheel_size)
+print(sedan.wheel_number)
+print(sedan.go())
+print(sedan.fill_up_tank())
--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,19 @@
 class Vehicle:
     pass
+
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    def go(self):
+        return "vrrrrrrrooom!"
+    
+    def fill_up_tank(self):
+        return "filling up!"
+
+
+merc = Vehicle(12, 13)
+print(merc.wheel_size)
+print(merc.wheel_number)
+print(merc.go())
+print(merc.fill_up_tank())
